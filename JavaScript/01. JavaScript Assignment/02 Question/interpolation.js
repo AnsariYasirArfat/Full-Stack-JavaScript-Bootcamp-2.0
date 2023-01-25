72a37af3ac9a9372782c9f811f712299b4531e1b
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-01-25 13:52:28 +0530

    02 Question file adde in JavaScript assignment

---
+++ JavaScript/01. JavaScript Assignment/02 Question/interpolation.js
@@ -0,0 +1,10 @@
+// Declare variables
+let firstName = "Yasir";
+let middleName = "Arfat"
+let lastName = "Ansari";
+let status = "single";
+let country = "India";
+let age = 23;
+
+// Display variables using interpolation
+console.log(`My name is ${lastName} ${firstName} ${middleName}. And I am ${age} years old from ${country}. My marital status is ${status}.`);
