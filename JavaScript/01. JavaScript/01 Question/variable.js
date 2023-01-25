9987107b736a1895be90c505121717efefbe0cd7
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-01-25 13:50:18 +0530

    01 Question file adde in JavaScript assignment

---
+++ JavaScript/01. JavaScript/01 Question/variable.js
@@ -0,0 +1,10 @@
+let string = 'Hi I am string';
+let boolean = true;
+let Undefined;
+let Null = null;
+
+// Display values and data types
+console.log(` '${string}', Data Type: ${typeof string}`);
+console.log(` ${boolean}, Data Type: ${typeof boolean}`);
+console.log(` ${Undefined}, Data Type: ${typeof Undefined}`);
+console.log(` ${Null}, Data Type: ${typeof Null}`);
