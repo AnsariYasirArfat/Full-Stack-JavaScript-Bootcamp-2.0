1fdae068f904cd03613bf68291b072f01c1509e5
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-01-27 08:02:41 +0530

    JS assingment 03 and 04 question added

---
+++ JavaScript/01. JavaScript Assignment/03 Question/uppercase.js
@@ -0,0 +1,9 @@
+// Declare a variable and assign a string value
+let string = "Make me to Upper Case";
+
+// Change all characters to uppercase
+let upperCase = string.toUpperCase();
+
+// Display the original message and the uppercase message
+console.log("Original string:", string);
+console.log("Converted to uppercase:", upperCase);
---
+++ JavaScript/01. JavaScript Assignment/04 Question/include.js
@@ -0,0 +1,13 @@
+// Declare a variable and assign a string value
+let string = "JavaScdript is a programming language";
+
+// Check if the string contains the word "Script"
+let checking = string.includes("Script");
+
+// Display the results
+if (checking === true) {
+    console.log(`string contains the word 'Script'`);
+} else {
+    console.log(`string does not contain the word 'Script'`)
+};
+
