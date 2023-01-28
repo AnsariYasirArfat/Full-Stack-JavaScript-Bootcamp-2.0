9c33beb11ff1611bfa3c9f922773f0295c491914
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-01-28 13:34:48 +0530

    JS assingment 06  question added

---
+++ JavaScript/01. JavaScript Assignment/06 Question/split.js
@@ -0,0 +1,6 @@
+let companiesString = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+
+let companyArray = companiesString.split(", ");
+
+// Display the array
+console.log("Companies:", companyArray);
