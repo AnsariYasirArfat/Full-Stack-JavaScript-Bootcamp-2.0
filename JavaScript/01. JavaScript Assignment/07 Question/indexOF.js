59f8176d40ca3686445b015e5812f106461455fc
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-01-28 13:35:56 +0530

    JS assingment 07  question added

---
+++ JavaScript/01. JavaScript Assignment/07 Question/indexOF.js
@@ -0,0 +1,8 @@
+let skills = ["iNeuron", "JavaScript", "pw skills",  "React", "Node.js", "pw skills", "MongoDB", "pw skills", "Git"];
+
+let firstOccurrence = skills.indexOf("pw skills");
+let lastOccurrence = skills.lastIndexOf("pw skills");
+
+// Display the position of the first and last occurrence of "pw skills"
+console.log(`The first occurrence of "pw skills" is at position ${firstOccurrence}`);
+console.log(`The last occurrence of "pw skills" is at position ${lastOccurrence}`);
