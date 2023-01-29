829b6e87d95ed77daabc462582f8a7650f76b364
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-01-29 11:02:18 +0530

    JS assingment 09  question added

---
+++ JavaScript/01. JavaScript Assignment/09 Question/boolean.js
@@ -0,0 +1,18 @@
+// Truthy Values
+
+let string = "Hello, world!";
+let number = 75;
+let object = {};
+console.log(`A non empty string = "${string}" is truthy value`); 
+console.log(`A non-zero number = ${number} is truthy value`);
+console.log(`An object = ${object} is truthy value`);
+
+
+// Falsy Values
+
+let stringEmpty = "";
+let numberZero = 0;
+let undefined;
+console.log(`A empty string = "${stringEmpty}" is falsy value`); 
+console.log(`A zero number = ${numberZero} is falsy value`);
+console.log(`${undefined} is falsy value`);
