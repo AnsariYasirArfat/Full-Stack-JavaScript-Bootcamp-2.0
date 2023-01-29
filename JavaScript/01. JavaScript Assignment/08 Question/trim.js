909e7be6c39ac4818ca6153f81235b8281152b1f
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-01-29 11:01:27 +0530

    JS assingment 08 question added

---
+++ JavaScript/01. JavaScript Assignment/08 Question/trim.js
@@ -0,0 +1,5 @@
+let string= "   Hello, World!   ";
+console.log(`A string without trim()method:   "${string}"`); // "   Hello, World!   "
+
+let stringTrim = string.trim();
+console.log(`A string with trim()method:   "${stringTrim}"`); // "Hello, World!"
