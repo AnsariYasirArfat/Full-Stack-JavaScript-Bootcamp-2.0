1e1abefc6347aacdee61cc8e4e163395e17c0f8b
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-01-29 11:03:03 +0530

    JS assingment 09  question added

---
+++ JavaScript/01. JavaScript Assignment/10 Question/comparision.js
@@ -0,0 +1,34 @@
+// The result of 4 > 3 is true.
+console.log(4 > 3);
+
+// The result of 4 >= 3 is true.
+console.log(4 >= 3);
+
+// The result of 4 < 3 is false.
+console.log(4 < 3);
+
+// The result of 4 <= 3 is false.
+console.log(4 <= 3);
+
+// The result of 4 == 4 is true.
+console.log(4 == 4);
+
+// The result of 4 === 4 is true.
+console.log(4 === 4);
+
+// The result of 4 != 4 is false.
+console.log(4 != 4);
+
+// The result of 4 !== 4 is false.
+console.log(4 !== 4);
+
+// The result of 4 != '4' is false.
+console.log(4 != '4');
+
+// The result of 4 == '4' is true.
+console.log(4 == '4');
+
+// The result of 4 === '4' is false.
+console.log(4 === '4');
+
+if ("python".length < "jargon".length) { console.log("This is a falsy statement") }
