d844ed01dbe3907eeb7c202312607e3a48435265
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-03-13 21:21:52 +0530

    getdata function to fetch weather api is addedd in js file

--- Projects/01. WeatherApp (Javascript)/script.js
+++ Projects/01. WeatherApp (Javascript)/script.js
@@ -8,28 +8,54 @@ const temprature = document.getElementById("temprature");
 const logoImage = document.getElementById("logoImage");
 const weatherStatus = document.getElementById("weatherStatus");
 
+
 const getData = async (event) => {
   event.preventDefault();
-  if (!inputBox.value) {
-    alert("Please Enter The City Name");
-    return;
-  }
   const city = inputBox.value;
-  // Fetch Details
 
-  const fetchData = await fetch(
-    `https://api.weatherapi.com/v1/current.json?key=aaa0929007ce456e8fb142153232602&q=${city}`
-  );
+  if (city) {
+    // Fetch Details from weather API
+    const fetchData = await fetch(
+      `http://api.weatherapi.com/v1/forecast.json?key=aaa0929007ce456e8fb142153232602&q=${city}&days=14`
+    );
 
-  const orgData = await fetchData.json();
-  console.log(orgData);
-  // Displaying the Details
-  countryName.innerHTML = orgData.location.country;
-  stateName.innerHTML = orgData.location.region;
-  cityName.innerHTML = city;
-  humidity.innerHTML = orgData.current.humidity;
-  windSpeed.innerHTML = orgData.current.wind_kph;
-  temprature.innerHTML = orgData.current.temp_c;
-  logoImage.src = orgData.current.condition.icon;
-  weatherStatus.innerHTML = orgData.current.condition.text;
+    orgData = await fetchData.json();
+    console.log(orgData);
+    // Current weather
+    displayCurrentWeatherData();
+    // Day report
+    todaysForecast = orgData.forecast.forecastday[0];
+    displayHourlyWeatherData();
+    displayTodayWeatherSummary();
+
+    // Forecast for next week
+    nextWeekForecast = orgData.forecast.forecastday;
+    displayForecastWeatherForWeek();
+  } else {
+    // Show Modal
+    const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
+    alertPlaceholder.innerHTML = `
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="staticBackdropLabel">Error</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+        <div class="modal-body">
+          Please enter a city name.
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>
+`;
+
+    const modal = new bootstrap.Modal(
+      document.getElementById("staticBackdrop")
+    );
+    modal.show();
+  }
 };
