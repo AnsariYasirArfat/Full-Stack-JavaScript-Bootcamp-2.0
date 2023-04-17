10a241e5d7f3aa6230e92b31debc105895f756f6
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-04-18 00:42:46 +0530

    bootstrap modell added js file of weather app project

--- Projects/01. WeatherApp (Javascript)/script.js
+++ Projects/01. WeatherApp (Javascript)/script.js
@@ -87,7 +87,23 @@ const getData = async (event) => {
   } else {
     // Show Modal
     const alertPlaceholder = document.getElementById("liveAlertPlaceholder");
-    alertPlaceholder.innerHTML = ``;
+    alertPlaceholder.innerHTML = `
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="staticBackdropLabel">Error</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+        <div class="modal-body">
+          Please enter a city name.
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        </div>
+      </div>
+    </div>
+  </div>`;
   }
 };
 
