571a7274e85201eb840f273d62331e3edbf6d973
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-04-29 08:11:10 +0530

    initialize the todos using useState hook in App.js Todo app project

--- Projects/02. Todo App_React & Bootstrap/todos-app/src/App.js
+++ Projects/02. Todo App_React & Bootstrap/todos-app/src/App.js
@@ -3,6 +3,9 @@ import { useState } from "react";
 
 function App() {
   const [filterText, setFilterText] = useState("");
+  const [todos, setTodos] = useState([
+    { todo: "first task todo", sno: 1, desc: "imp task need to be done quick" },
+  ]);
 
   return (
     <>
