cdd8a81eb1dfd9669314c76567c898b1b27ac936
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-05-02 11:58:54 +0530

    Imported Header.css & CSS class updated in Header.js of Todo app project

--- Projects/02. Todo App_React & Bootstrap/todos-app/src/Components/Header.js
+++ Projects/02. Todo App_React & Bootstrap/todos-app/src/Components/Header.js
@@ -1,28 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 import DisplayDate from "./DisplayDate";
+import "../CSS/Header.css";
 import mainLogo from "../assets/mainLogo.png";
 import searchInList from "../assets/icons8-search-in-list-24.png";
 
 Header.propTypes = {
   title: PropTypes.string,
-  searchBar: PropTypes.bool,
 };
 Header.defaultProps = {
   title: "Your Title Here",
-  searchBar: false,
 };
 
 function Header(props) {
   return (
     <>
-      <nav className="navbar text-center py-3">
-        <div className="container-fluid row row-cols-1 row-cols-sm-2 row-cols-lg-3 m-auto">
+      <nav className="navbar z-3 text-center py-3 position-sticky top-0">
+        <div className="container-fluid row row-cols-1 row-cols-md-2 row-cols-lg-3 m-auto">
           {/* Name & Logo */}
-          <div className="p-2 col d-flex justify-content-center align-items-center">
-            <h1 className="fw-bold fs-1 title me-3">{props.title}</h1>
+          <div className="pe-1 col d-flex justify-content-center align-items-center">
+            <div className="fw-bold title me-3">{props.title}</div>
             <div>
-              <img alt="Logo" width="45" src={mainLogo} />
+              <img alt="Logo" width="45" src={mainLogo} className="mainLogo" />
             </div>
           </div>
           {/* Input Form for Searhing task  */}
@@ -31,12 +30,12 @@ function Header(props) {
               onSubmit={(e) => {
                 e.preventDefault();
               }}
-              className="py-5 col"
+              className=" m-auto searchForm d-flex justify-content-evenly align-items-center"
               role="search"
             >
               <img src={searchInList} alt="serch" className="searchLogo" />
               <input
-                className="form-control inputBox m-auto py-2"
+                className="form-control inputBox  "
                 type="search"
                 value={props.filterText}
                 placeholder="Search your assigned tasks here...."
