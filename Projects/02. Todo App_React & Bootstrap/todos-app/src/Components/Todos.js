52e452735ee2b4d8a53ca487cf6cd47b0243d9c3
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-05-07 12:15:16 +0530

    two section introduce for task in todos.js

--- Projects/02. Todo App_React & Bootstrap/todos-app/src/Components/Todos.js
+++ Projects/02. Todo App_React & Bootstrap/todos-app/src/Components/Todos.js
@@ -6,23 +6,56 @@ const Todos = (props) => {
   };
 
   return (
-    <div className="container my-3" style={myStyle}>
-      <h3 className=" m-3">Todos list</h3>
+    <div style={myStyle}>
+      <h3>Todos list</h3>
       <div>
-        <p>Totol task Remain {props.todos.length}</p>
-        {props.todos.length === 0
-          ? " No todos Here"
-          : remainTask.map((todo) => {
-              return (
-                <TodoItem
-                  todo={todo}
-                  key={todo.sno}
-                  onDelete={props.onDelete}
-                  onEdit={props.onEdit}
-                  onChecked={props.onChecked}
-                />
-              );
-            })}
+        {/* Remaining tasks Section  */}
+        <div>
+          <p>
+            Tasks Remaining: <span> {remainTask.length}</span>
+          </p>
+          <div>
+            {remainTask.length === 0 ? (
+              <h5>No Remaining task here!</h5>
+            ) : (
+              remainTask.map((todo) => {
+                return (
+                  <TodoItem
+                    todo={todo}
+                    key={todo.sno}
+                    onDelete={props.onDelete}
+                    onEdit={props.onEdit}
+                    onChecked={props.onChecked}
+                  />
+                );
+              })
+            )}
+          </div>
+        </div>
+        {/* Completed tasks Section */}
+        <div>
+          <p>
+            Finished Tasks:
+            <span> {completedTask.length}</span>
+          </p>
+          <div>
+            {completedTask.length === 0 ? (
+              <h5>No Completed task here!</h5>
+            ) : (
+              completedTask.map((todo) => {
+                return (
+                  <TodoItem
+                    todo={todo}
+                    key={todo.sno}
+                    onDelete={props.onDelete}
+                    onEdit={props.onEdit}
+                    onChecked={props.onChecked}
+                  />
+                );
+              })
+            )}
+          </div>
+        </div>
       </div>
     </div>
   );
