4c799aac3a27a7453580d0de6f2700f9c785bae4
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-05-08 06:59:17 +0530

    edit task functionality added  in TodoItem.js

--- Projects/02. Todo App_React & Bootstrap/todos-app/src/Components/TodoItem.js
+++ Projects/02. Todo App_React & Bootstrap/todos-app/src/Components/TodoItem.js
@@ -1,53 +1,107 @@
 import React, { useState } from "react";
 
 const Todoitem = ({ todo, onDelete, onEdit, onChecked }) => {
-  return (
-    <>
-      <div>
+  const [editing, setEditing] = useState(false);
+  let todoItem;
+
+  if (editing) {
+    todoItem = (
+      <>
         <div>
-          <input
-            type="checkbox"
-            checked={todo.done}
-            onChange={(e) => {
-              onChecked({
-                ...todo,
-                done: e.target.checked,
-              });
-              checkedBox();
-            }}
-          />
-          <h5>{todo.title}</h5>
-        </div>
-        <div>
-          <p>{todo.desc}</p>
-          <small>{todo.todoDate + " at " + todo.todoTime}</small>
-        </div>
-        <div>
-          {!DescriptionState && (
-            <div>
-              <small>{todo.todoDate + " at " + todo.todoTime}</small>
-            </div>
-          )}
           <div>
-            {todo.done === false && (
-              <button onClick={() => setEditing(true)}>
+            <h5 style={{ color: "#0d6caf" }}>Edit Your Task:</h5>
+            <button onClick={() => onSave()}>
+              <img
+                src="https://img.icons8.com/3d-fluency/94/null/save.png"
+                alt="Save"
+                width={30}
+              />
+              Save!
+            </button>
+          </div>
+          <div>
+            <input
+              maxlength="50"
+              required
+              value={todo.title}
+              placeholder="No Title"
+              onChange={(e) => {
+                onEdit({
+                  ...todo,
+                  title: e.target.value,
+                });
+              }}
+            />
+          </div>
+          <div>
+            <textarea
+              value={todo.desc}
+              placeholder="No Description"
+              onChange={(e) => {
+                onEdit({
+                  ...todo,
+                  desc: e.target.value,
+                });
+              }}
+            ></textarea>
+          </div>
+        </div>
+      </>
+    );
+  } else {
+    todoItem = (
+      <>
+        <div>
+          <div>
+            <input
+              type="checkbox"
+              checked={todo.done}
+              onChange={(e) => {
+                onChecked({
+                  ...todo,
+                  done: e.target.checked,
+                });
+                checkedBox();
+              }}
+            />
+            <h5>{todo.title}</h5>
+          </div>
+          <div>
+            <p>{todo.desc}</p>
+            <small>{todo.todoDate + " at " + todo.todoTime}</small>
+          </div>
+          <div>
+            {!DescriptionState && (
+              <div>
+                <small>{todo.todoDate + " at " + todo.todoTime}</small>
+              </div>
+            )}
+            <div>
+              {todo.done === false && (
+                <button onClick={() => setEditing(true)}>
+                  <img
+                    src="https://img.icons8.com/3d-fluency/94/null/edit.png"
+                    alt="Edit"
+                    width={30}
+                  />
+                </button>
+              )}
+              <button onClick={() => onDelete(todo)}>
                 <img
-                  src="https://img.icons8.com/3d-fluency/94/null/edit.png"
-                  alt="Edit"
+                  src="https://img.icons8.com/plasticine/100/null/filled-trash.png"
+                  alt="Delete"
                   width={30}
                 />
               </button>
-            )}
-            <button onClick={() => onDelete(todo)}>
-              <img
-                src="https://img.icons8.com/plasticine/100/null/filled-trash.png"
-                alt="Delete"
-                width={30}
-              />
-            </button>
+            </div>
           </div>
         </div>
-      </div>
+      </>
+    );
+  }
+  return (
+    <>
+      <div className="p-0 ">{todoItem}</div>
     </>
   );
 };
