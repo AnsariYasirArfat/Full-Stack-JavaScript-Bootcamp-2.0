124adde8302bf3453217c1fa0aaa40aab9f6fbf6
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-05-12 11:41:07 +0530

    TodoItem.css file added & imported in TodoItem.js

--- Projects/02. Todo App_React & Bootstrap/todos-app/src/Components/TodoItem.js
+++ Projects/02. Todo App_React & Bootstrap/todos-app/src/Components/TodoItem.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Collapse from "react-bootstrap/Collapse";
+import "../CSS/TodoItem.css";
 
 const Todoitem = ({
   todo,
