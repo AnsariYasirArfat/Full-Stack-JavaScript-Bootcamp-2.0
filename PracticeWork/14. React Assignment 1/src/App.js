40c8fada7a62150c58a0af3b0bbdda490f6429a0
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-05-23 11:03:20 +0530

    Reset & DecrementByValue buttons & styles added in App.js in React-Assignment-1 Counter App

--- PracticeWork/14. React Assignment 1/src/App.js
+++ PracticeWork/14. React Assignment 1/src/App.js
@@ -1,10 +1,11 @@
-import { immerable } from "immer";
 import React, { useState } from "react";
 
 import {
   increment,
   decrement,
   incrementByValue,
+  decrementByValue,
+  reset,
 } from "./features/counter/counterSlice";
 
 import { useSelector, useDispatch } from "react-redux";
@@ -15,21 +16,7 @@ function App() {
   const dispatch = useDispatch();
   return (
     <div className="bg-zinc-900 text-zinc-100 flex justify-center items-center flex-col min-h-screen gap-6">
-      <div className="space-x-3">
-        <input
-          type="number"
-          className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-          placeholder="Enter a number..."
-          value={Input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button
-          onClick={() => dispatch(incrementByValue(+Input))}
-          className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-        >
-          Increment by Value
-        </button>
-      </div>
+      <span className="text-9xl">{count}</span>
       <div className="space-x-6">
         <button
           onClick={() => dispatch(increment())}
@@ -37,7 +24,6 @@ function App() {
         >
           Increment by 1
         </button>
-        <span>{count}</span>
         <button
           onClick={() => dispatch(decrement())}
           className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
@@ -45,6 +31,43 @@ function App() {
           Decrement by 1
         </button>
       </div>
+
+      <div className="space-x-3">
+        <input
+          type="number"
+          className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          placeholder="Enter a number..."
+          value={Input1}
+          onChange={(e) => setInput1(e.target.value)}
+        />
+        <button
+          onClick={() => dispatch(incrementByValue(+Input1))}
+          className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+        >
+          Increment by Value
+        </button>
+      </div>
+      <div className="space-x-3">
+        <button
+          onClick={() => dispatch(decrementByValue(+Input2))}
+          className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+        >
+          Decrement by Value
+        </button>
+        <input
+          type="number"
+          className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+          placeholder="Enter a number..."
+          value={Input2}
+          onChange={(e) => setInput2(e.target.value)}
+        />
+      </div>
+      <button
+        onClick={() => dispatch(reset())}
+        className="text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg"
+      >
+        Reset
+      </button>
     </div>
   );
 }
