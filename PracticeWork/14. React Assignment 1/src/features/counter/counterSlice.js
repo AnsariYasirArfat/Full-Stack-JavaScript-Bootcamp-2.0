5f3e987296e072b80f7fd65c063d40e07b4c25a7
AnsariYasirArfat <ansariyaseer786@gmail.com>
2023-05-23 11:00:13 +0530

    Reset & DecrementByValue functionality added in counterSlice.js in React-Assignment-1 Counter App

--- PracticeWork/14. React Assignment 1/src/features/counter/counterSlice.js
+++ PracticeWork/14. React Assignment 1/src/features/counter/counterSlice.js
@@ -17,8 +17,20 @@ export const counterSlice = createSlice({
     incrementByValue: (state, action) => {
       state.value += action.payload;
     },
+    decrementByValue: (state, action) => {
+      state.value -= action.payload;
+    },
+    reset: (state) => {
+      state.value = 0;
+    },
   },
 });
 
-export const { increment, decrement, incrementByValue } = counterSlice.actions;
+export const {
+  increment,
+  decrement,
+  incrementByValue,
+  decrementByValue,
+  reset,
+} = counterSlice.actions;
 export default counterSlice.reducer;
